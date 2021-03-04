2d0ef51310485c5183bc0f1176900c16e14d46a7
karthikbhagi2727 <16j41a0211@gmail.com>
2021-03-04 20:10:51 +0530

    2a completed

--- index.js
+++ index.js
@@ -15,3 +15,55 @@ let array = [
   array.map((data,index)=>{
         console.log([data.toLowerCase(), data.substring(0,3), data.length])
   })
+
+  /*calculate greater than 85*/
+  const users = [
+    {
+        name:'Brook', 
+        scores:75,
+        skills:['HTM', 'CSS', 'JS'],
+        age:16
+    },
+    {
+        name:'Alex', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    }, 
+    {
+        name:'David', 
+        scores:75,
+        skills:['HTM', 'CSS'],
+        age:22
+    }, 
+    {
+        name:'John', 
+        scores:85,
+        skills:['HTM'],
+        age:25
+    },
+    {
+        name:'Sara',
+        scores:95,
+        skills:['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name:'Martha', 
+        scores:80,
+        skills:['HTM', 'CSS', 'JS'],
+        age:18
+    },
+    {
+        name:'Thomas',
+        scores:90,
+        skills:['HTM', 'CSS', 'JS'],
+        age:20
+    }
+    ];
+
+    users.map((data,index)=>{
+        if(data.scores > 85){
+            console.log(data);
+        }
+    });
